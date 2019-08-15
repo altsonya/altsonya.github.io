a47359e8ddb66b8a0b42461a26707ebe471d6576
Gennadiy <genageneralov@gmail.com>
2019-08-15 20:00:26 +0300

    Update 2019-08-15T17:00:23.663Z

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-$(window).load(function(){$(".loaderInner").fadeOut(),$(".loader").delay(400).fadeOut("slow")});
+$(window).load(function() { 
+	$(".loaderInner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
+});
+
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(54924259, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
